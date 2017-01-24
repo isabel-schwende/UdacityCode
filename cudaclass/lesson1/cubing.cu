537a3760da17b88ed44aa6c36c3962637202daba
Isabel <isabelschwende@yahoo.de>
2017-01-24 15:49:49 +0900

    Updated to my own solution
    Evaluated as correct

--- cudaclass/lesson1/cubing.cu
+++ cudaclass/lesson1/cubing.cu
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 __global__ void cube(float * d_out, float * d_in){
-	// Todo: Fill in this function
+	// modified function
+	int idx = threadIdx.x;
+	float f = d_in[idx];
+	d_out[idx] = f * f * f;
 }
 
 int main(int argc, char ** argv) {
-	const int ARRAY_SIZE = 64;
+	// changed number of bytes
+	const int ARRAY_SIZE = 96;
 	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
 
 	// generate the input array on the host
